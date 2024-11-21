f9dd275ca2469f1bd07d8f7f0ace63e3e83940bf
Mark Twombley <mtwombley1@gmail.com>
2024-11-20 19:11:37 -0700

    Change PLA Temp

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2426,7 +2426,7 @@
 // Preheat Constants - Up to 10 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 220
+#define PREHEAT_1_TEMP_HOTEND 210
 #define PREHEAT_1_TEMP_BED     55
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
